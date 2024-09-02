cfc60eaa965d86e02859841df56915bd2bc2ea12
WiQuert <roshev@slaps.ru>
2024-09-02 12:37:57 +0300

    Add bottom nav menu

--- app/src/main/java/com/example/onlinestore/MainActivity.kt
+++ app/src/main/java/com/example/onlinestore/MainActivity.kt
@@ -6,11 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.onlinestore.navigation.BottomNavItem
+import com.example.onlinestore.navigation.BottomNavItems
+import com.example.onlinestore.screens.Home
 import com.example.onlinestore.ui.theme.OnlineStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +27,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OnlineStoreTheme {
-
+                Home()
             }
         }
     }
