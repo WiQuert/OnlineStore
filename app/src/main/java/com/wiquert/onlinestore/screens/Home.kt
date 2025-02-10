a8b80219a5dc99d20748018f9571536f707958fa
WiQuert <roshev@slaps.ru>
2025-02-10 18:41:46 +0300

    Rewrote the deprecated code in the SearchBar (

--- app/src/main/java/com/wiquert/onlinestore/screens/Home.kt
+++ app/src/main/java/com/wiquert/onlinestore/screens/Home.kt
@@ -1,6 +1,5 @@
 package com.wiquert.onlinestore.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,64 +7,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.wiquert.onlinestore.R
-import com.wiquert.onlinestore.retrofit.MainApi
 import com.wiquert.onlinestore.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@ExperimentalMaterial3Api
 @Composable
 fun HomeScreen() {
         Scaffold (
             topBar = {
-                val searchText = remember {
-                    mutableStateOf("")
-                }
-
-                val isActive = remember {
-                    mutableStateOf(false)
-                }
-
-                SearchBar(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 10.dp),
-                    query = searchText.value,
-                    onQueryChange ={ text ->
-                        searchText.value = text
-
-                    },
-                    placeholder = {
-                        Text(text = stringResource(id = R.string.search_text))
-                    },
-                    onSearch = { text ->
-                        isActive.value = false
-                    },
-                    active = isActive.value,
-                    onActiveChange = {
-                        isActive.value = true
-                    }
-                ) {
-                    Text("Test")
-                    }
+                HomeSearchBar()
             }
         )
         { innerPadding ->
@@ -77,6 +40,51 @@ fun HomeScreen() {
                 }
             }
     }
+
+
+@Composable
+@ExperimentalMaterial3Api
+fun HomeSearchBar() {
+
+    val searchText = remember {
+        mutableStateOf("")
+    }
+
+    val isActive = remember {
+        mutableStateOf(false)
+    }
+
+    SearchBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = searchText.value,
+                onQueryChange = { text ->
+                    searchText.value = text
+                },
+                expanded = isActive.value,
+                onExpandedChange = { value ->
+                    isActive.value = value
+                },
+                onSearch = {
+                    isActive.value = false
+                },
+                placeholder = {
+                    Text("Type your search here")
+                }
+            )
+        },
+        expanded = isActive.value,
+        onExpandedChange = { value ->
+            isActive.value = value
+        },
+    )
+    {
+        Text("Coming soon")
+    }
+}
 
 
 @Composable
