48f27b95d4583051dad29a6bc9db3ddf6f7510f1
WiQuert <roshev@slaps.ru>
2025-03-26 20:09:02 +0300

    Added working LazyColumn

--- app/src/main/java/com/wiquert/onlinestore/screens/Home.kt
+++ app/src/main/java/com/wiquert/onlinestore/screens/Home.kt
@@ -1,6 +1,7 @@
 package com.wiquert.onlinestore.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,16 +9,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,8 +45,8 @@ fun HomeScreen() {
             Column (modifier = Modifier
                 .fillMaxWidth()
                 .padding(innerPadding)
-                .background(color = Color.Red)) {
-                    ShowAllProducts()
+            ) {
+                    ShowInterestingProducts()
                 }
         }
 
@@ -90,18 +99,25 @@ fun HomeSearchBar() {
 
 
 @Composable
-fun ShowAllProducts(viewModel: MainViewModel = hiltViewModel()) {
+fun ShowInterestingProducts(viewModel: MainViewModel = hiltViewModel()) {
+    val interestingProduct = viewModel.interestingProducts.value
     LazyColumn(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxSize(0.8f)
-        .padding(start = 5.dp, end = 5.dp, top = 15.dp)
+        .fillMaxSize()
+//        .fillMaxSize(0.8f)
+        .padding(start = 5.dp, end = 5.dp, top = 10.dp, bottom = 5.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        items(count = 20) {
-            Text(text = viewModel.name.value, fontSize = 18.sp)
-            Spacer(Modifier.size(15.dp))
-            Text(text = viewModel.description.value)
-            Spacer(Modifier.size(15.dp))
-            Text(text = "Total price: ${viewModel.price.value} USD")
+        items(interestingProduct) {item ->
+            Card {
+                Column(modifier = Modifier
+                    .padding(5.dp)) {
+                    Text(text = item.title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                    Spacer(Modifier.size(15.dp))
+                    Text(text = item.description)
+                    Spacer(Modifier.size(15.dp))
+                    Text(text = "Total price: $${item.price}")
+                       }
+                }
         }
     }
 }
--- app/src/main/java/com/wiquert/onlinestore/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/wiquert/onlinestore/viewmodel/MainViewModel.kt
@@ -16,11 +16,14 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val mainApi: MainApi) : ViewModel() {
 
     private val _products = mutableStateOf<List<Product>>(emptyList())
+    private val _interestingProducts = mutableStateOf<List<Product>>(emptyList())
     val products: State<List<Product>> = _products
+    val interestingProducts: State<List<Product>> = _interestingProducts
     init {
         viewModelScope.launch(Dispatchers.IO) {
             val allProductsList : List<Product> = mainApi.getAllProducts().products
             _products.value = allProductsList
+            _interestingProducts.value = allProductsList.shuffled().take(15)
         }
     }
 }
